29ebb3f650e79b1e02909f5be72fbd1ad8960c19
Melissa Tan <32193089+melxtan@users.noreply.github.com>
2025-04-15 21:05:45 -0700

    Update rdbms_connector.py

--- src/db/rdbms_connector.py
+++ src/db/rdbms_connector.py
@@ -4,4 +4,4 @@ import pymysql
 
 def connect_to_rdbms():
     """Establishes a connection to an RDBMS."""
-    return pymysql.connect(host="localhost", user="root", password="Dsci-551", database="dsci551")
+    return pymysql.connect(host="3.148.188.211", user="root", password="Dsci-551", database="dsci551")
