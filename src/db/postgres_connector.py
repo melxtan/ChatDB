6c415b44a534427c064f18fb64d4f7345295f7bd
Melissa Tan <32193089+melxtan@users.noreply.github.com>
2025-04-15 23:42:40 -0700

    Update postgres_connector.py

--- src/db/postgres_connector.py
+++ src/db/postgres_connector.py
@@ -6,7 +6,7 @@ from psycopg2.extras import RealDictCursor
 def connect_to_postgres():
     """Establishes a connection to PostgreSQL database."""
     return psycopg2.connect(
-        host="3.145.52.120",
+        host="18.224.56.248",
         database="dvdrental",
         user="postgres",
         password="postgres",
