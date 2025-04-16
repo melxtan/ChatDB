08ba2260ddf819442f7dae60330009d8c99c389e
Melissa Tan <32193089+melxtan@users.noreply.github.com>
2025-04-15 23:03:42 -0700

    Update nosql_connector.py

--- src/db/nosql_connector.py
+++ src/db/nosql_connector.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 def connect_to_nosql():
     """Establishes a connection to a NoSQL database."""
     try:
-        client = MongoClient("mongodb://3.148.188.211:27017/")
+        client = MongoClient("mongodb://3.145.52.120:8501/")
         # Test the connection
         client.server_info()
         db = client["world"]  # Use the existing database
